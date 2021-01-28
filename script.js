2713e61678d7510714b82225b147a416e333bf55
Dameon Davis <dad1977ut@yahoo.com>
2021-01-28 16:12:35 -0700

    added forecast

--- script.js
+++ script.js
@@ -28,22 +28,42 @@ function displayCity(city) {
         url: newQueryURL,
         method: "GET",
       }).then(function (response) {
+        console.log(response);
         $(".icon").empty();
+        $("#forecast").empty();
         var icon = response.current.weather[0].icon;
         var alt = response.current.weather[0].description;
         $(".city").text(city);
         console.log(alt);
         $(".icon").append(
-          "<img src=https://openweathermap.org/img/w/" +
-            icon +
-            ".png alt=" +
-            alt +
-            ">"
+          "<img src=https://openweathermap.org/img/w/" + icon + ".png>"
         );
         $(".temp").text(response.current.temp);
         $(".humidity").text(response.current.humidity);
         $(".wind").text(response.current.wind_speed);
         $(".uv").text(response.current.uvi);
+        var date = new Date(response.daily[1].dt * 1000);
+        var forecastIcon = response.daily[1].weather[0].icon;
+        console.log(forecastIcon);
+        $("#forecast").append(
+          `<div class="col-2">
+            <div class="card">
+              <div class="card-body p-1">
+                <h5 class="card-title">${
+                  date.getMonth() +
+                  1 +
+                  "/" +
+                  date.getDate() +
+                  "/" +
+                  date.getFullYear()
+                }</h5>
+                <img src=https://openweathermap.org/img/w/${forecastIcon}.png>
+                <p class="card-text">${response.daily[1].temp.day}</p>
+                <p class="card-text">${response.daily[1].humidity}</p>
+              </div>
+            </div>
+          </div>`
+        );
       });
     });
 }
